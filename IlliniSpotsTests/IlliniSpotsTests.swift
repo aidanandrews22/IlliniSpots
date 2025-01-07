894d470b0f69ceaf52650a1c88743a68c6542886
Aidan Andrews <aidansa2@illinois.edu>
2025-01-07 16:02:23 -0600

    Initial Commit

---
+++ IlliniSpotsTests/IlliniSpotsTests.swift
@@ -0,0 +1,17 @@
+//
+//  IlliniSpotsTests.swift
+//  IlliniSpotsTests
+//
+//  Created by Aidan Andrews on 1/7/25.
+//
+
+import Testing
+@testable import IlliniSpots
+
+struct IlliniSpotsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
